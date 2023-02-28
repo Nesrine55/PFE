79b9ba8835b8a40c1b6679e7890ed98c408a8ff3
nesrine zarrouk <nesrinezarrouk83@gmail.com>
2023-02-28 12:40:31 +0100

    feat : home page fix, add: resend button il mailvalidate component

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,18 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class HomepageComponent implements AfterViewInit{
   ngAfterViewInit(): void {
 
 
-    const images = ['/assets/home/pic1.jpg', '/assets/home/pic2.jpeg', '/assets/home/pic3.png', '/assets/home/pic4.jpg'];
+    const images = ['./assets/home/1.jpg', './assets/home/2.jpg', './assets/home/3.jpg', './assets/home/4.jpg', './assets/home/5.jpg', './assets/home/6.jpg', './assets/home/7.jpg'];
     let i = 0;
     const sliderImg = document.querySelector('.sliderImg') as HTMLImageElement;
     var len = images.length;
